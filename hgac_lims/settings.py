35cc9c4e9fbb87c44d024074b2c27437761c8672
Jason Grundstad <jgrundstad@gmail.com>
2015-10-21 12:12:40 -0500

    added MySQL to env and site settings

--- hgac_lims/settings.py
+++ hgac_lims/settings.py
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'hgac_lims.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'hgac_lims', 'my.cnf'),
+        },
     }
 }
 
