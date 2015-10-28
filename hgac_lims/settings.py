dfe3059041a2d24ac9aa26eff88b1f6fe191e13d
Jason Grundstad <jgrundstad@gmail.com>
2015-10-28 09:17:45 -0500

    added TEMPLATE_DIRS to TEMPLATES dict, req'd 1.8.3

--- hgac_lims/settings.py
+++ hgac_lims/settings.py
@@ -110,4 +110,4 @@ STATIC_PATH = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
+TEMPLATES = {'TEMPLATE_DIRS': os.path.join(BASE_DIR,  'templates'), }
